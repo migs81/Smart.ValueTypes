4ecaa8d14b1646904a16ba79a48d7f48e7054526
migs81 <m.gschwandtner81@gmail.com>
2026-07-06 16:42:42 +0200

    perf: add benchmarks for HSL color model

---
+++ benchmarks/ValueTypes.Benchmarks/ColorModels/HSLBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Migs.ValueTypes.Types.ColorModels;
+
+namespace Migs.ValueTypes.Benchmarks.ColorModels
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class HSLBenchmarks
+    {
+        [Benchmark]
+        public void HSL_Constructor_Benchmark()
+        {
+            _ = new HSL(1, 1, 1);
+        }
+        
+        [Benchmark]
+        public void HSL_From_Benchmark()
+        {
+            _ = HSL.From(1, 1, 1);
+        }
+
+        [Benchmark]
+        public void HSL_TryFrom_Benchmark()
+        {
+            _ = HSL.TryFrom(1, 1, 1, out _);
+        }
+    }
+}
