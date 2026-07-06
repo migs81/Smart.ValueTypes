0c71c1d67b717343783d5eda78468627f72da6e9
migs81 <m.gschwandtner81@gmail.com>
2026-07-06 20:32:52 +0200

    perf: add benchmarks for HSV value type

---
+++ benchmarks/ValueTypes.Benchmarks/ColorModels/HSVBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Migs.ValueTypes.Types.ColorModels;
+
+namespace Migs.ValueTypes.Benchmarks.ColorModels
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class HSVBenchmarks
+    {
+        [Benchmark]
+        public void HSV_Constructor_Benchmark()
+        {
+            _ = new HSV(1, 1, 1);
+        }
+        
+        [Benchmark]
+        public void HSV_From_Benchmark()
+        {
+            _ = HSV.From(1, 1, 1);
+        }
+
+        [Benchmark]
+        public void HSV_TryFrom_Benchmark()
+        {
+            _ = HSV.TryFrom(1, 1, 1, out _);
+        }
+    }
+}
