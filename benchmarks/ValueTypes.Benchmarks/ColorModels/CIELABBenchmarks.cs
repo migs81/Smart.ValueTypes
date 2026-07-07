d5f4c0cbae56b1a10c6c24d034e76ec5607209a0
migs81 <m.gschwandtner81@gmail.com>
2026-07-07 19:07:11 +0200

    perf: add benchmark for value type CIELAB

---
+++ benchmarks/ValueTypes.Benchmarks/ColorModels/CIELABBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.ColorModels;
+
+namespace Smart.ValueTypes.Benchmarks.ColorModels
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class CIELABBenchmarks
+    {
+        [Benchmark]
+        public void CIELAB_Constructor_Benchmark()
+        {
+            _ = new CIELAB(1, 1, 1);
+        }
+        
+        [Benchmark]
+        public void CIELAB_From_Benchmark()
+        {
+            _ = CIELAB.From(1, 1, 1);
+        }
+
+        [Benchmark]
+        public void CIELAB_TryFrom_Benchmark()
+        {
+            _ = CIELAB.TryFrom(1, 1, 1, out _);
+        }
+    }
+}
