3fcfbd3db0829e7f783d97acda34e8f229a1a108
migs81 <m.gschwandtner81@gmail.com>
2026-07-07 22:00:24 +0200

    perf: add benchmark for value type YCbCr

---
+++ benchmarks/ValueTypes.Benchmarks/ColorModels/YCbCrBBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.ColorModels;
+
+namespace Smart.ValueTypes.Benchmarks.ColorModels
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class YCbCrBenchmarks
+    {
+        [Benchmark]
+        public void YCbCr_Constructor_Benchmark()
+        {
+            _ = new YCbCr(1, 0.5f, 0.5f);
+        }
+        
+        [Benchmark]
+        public void YCbCr_From_Benchmark()
+        {
+            _ = YCbCr.From(1, 0.5f, 0.5f);
+        }
+
+        [Benchmark]
+        public void YCbCr_TryFrom_Benchmark()
+        {
+            _ = YCbCr.TryFrom(1, 0.5f, 0.5f, out _);
+        }
+    }
+}
