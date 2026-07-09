6be2a2433dbe127ba4cdb4e855c9116b4ce32f73
migs81 <m.gschwandtner81@gmail.com>
2026-07-09 21:38:45 +0200

    perf: add benchmarks for value type BoundedNumber

---
+++ benchmarks/ValueTypes.Benchmarks/Numeric/BoundedNumberBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Numeric;
+
+namespace Smart.ValueTypes.Benchmarks.Numeric
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class BoundedNumberBenchmarks
+    {
+        [Benchmark]
+        public void BoundedNumber_Constructor_Benchmark()
+        {
+            _ = new BoundedNumber<int>(1, 1, 1);
+        }
+        
+        [Benchmark]
+        public void BoundedNumber_From_Benchmark()
+        {
+            _ = BoundedNumber<int>.From(1, 1, 1);
+        }
+
+        [Benchmark]
+        public void BoundedNumber_TryFrom_Benchmark()
+        {
+            _ = BoundedNumber<int>.TryFrom(1, 1, 1, out _);
+        }
+    }
+}
