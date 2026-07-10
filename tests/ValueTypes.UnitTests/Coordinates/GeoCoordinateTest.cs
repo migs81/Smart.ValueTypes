dcdb681ea10a66445e4224be6683ffe0ea22c617
migs81 <m.gschwandtner81@gmail.com>
2026-07-10 17:35:53 +0200

    test: add unit test for the value type GeoCoordinate

---
+++ tests/ValueTypes.UnitTests/Coordinates/GeoCoordinateTest.cs
@@ -0,0 +1,38 @@
+﻿using Smart.ValueTypes.Types.Coordinates;
+using Xunit;
+
+namespace Smart.ValueTypes.UnitTests.Coordinates
+{
+    public class GeoCoordinateTest
+    {
+        #region constructor
+
+        [Fact]
+        public void Constructor_NoInput_ShouldReturnDefaultObject()
+        {
+            // act
+            var result = new GeoCoordinate();
+            
+            // assert
+            Assert.Equal(Latitude.Empty, result.Latitude, 0);
+            Assert.Equal(Longitude.Empty, result.Longitude, 0);
+        }
+
+        [Fact]
+        public void Constructor_ValidInput_ShouldReturnObject()
+        {
+            // arrange
+            var longitude = new Longitude(10);
+            var latitude = new Latitude(10);
+            
+            // act
+            var result = new GeoCoordinate(longitude, latitude);
+            
+            // assert
+            Assert.Equal(latitude, result.Latitude);
+            Assert.Equal(longitude, result.Longitude);
+        }
+
+        #endregion
+    }
+}
