e565f478d47fbd9a856508575b4383ced1af6ee7
migs81 <m.gschwandtner81@gmail.com>
2026-07-10 17:26:13 +0200

    perf: add benchmarks for the value type NumberRange

---
+++ benchmarks/ValueTypes.Benchmarks/Numeric/NumberRangeBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Numeric;
+
+namespace Smart.ValueTypes.Benchmarks.Numeric
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class NumberRangeBenchmarks
+    {
+        [Benchmark]
+        public void NumberRange_Constructor_Benchmark()
+        {
+            _ = new NumberRange<int>(0, 10);
+        }
+        
+        [Benchmark]
+        public void NumberRange_From_Benchmark()
+        {
+            _ = NumberRange<int>.From(0, 10);
+        }
+
+        [Benchmark]
+        public void NumberRange_TryFrom_Benchmark()
+        {
+            _ = NumberRange<int>.TryFrom(0, 10, out _);
+        }
+    }
+}
