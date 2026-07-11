2201b64c662b4fca8d5ddb342dcf40f94f1f36e5
migs81 <m.gschwandtner81@gmail.com>
2026-07-11 16:38:34 +0200

    perf: add benchmarks for SVNR value type

---
+++ benchmarks/ValueTypes.Benchmarks/NationalInsuranceNumbers/SVNRBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.NationalInsuranceNumbers;
+
+namespace Smart.ValueTypes.Benchmarks.NationalInsuranceNumbers
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class SVNRBenchmarks
+    {
+        [Benchmark]
+        public void SVNR_Constructor_Benchmark()
+        {
+            _ = new SVNR("7123060697");
+        }
+        
+        [Benchmark]
+        public void SVNR_From_Benchmark()
+        {
+            _ = SVNR.From("7123060697");
+        }
+
+        [Benchmark]
+        public void SVNR_TryFrom_Benchmark()
+        {
+            _ = SVNR.TryFrom("7123060697", out _);
+        }
+    }
+}
