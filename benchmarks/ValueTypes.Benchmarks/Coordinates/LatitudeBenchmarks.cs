888c8b113ef03816b70c75b1068d7027059302a2
migs81 <m.gschwandtner81@gmail.com>
2026-07-11 20:27:59 +0200

    perf: add benchmarks for Latitude and Longitude value types

---
+++ benchmarks/ValueTypes.Benchmarks/Coordinates/LatitudeBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Coordinates;
+
+namespace Smart.ValueTypes.Benchmarks.Coordinates
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class LatitudeBenchmarks
+    {
+        [Benchmark]
+        public void Latitude_Constructor_Benchmark()
+        {
+            _ = new Latitude(0);
+        }
+
+        [Benchmark]
+        public void Latitude_From_Benchmark()
+        {
+            _ = Latitude.From(0);
+        }
+    }
+}
---
+++ benchmarks/ValueTypes.Benchmarks/Coordinates/LongitudeBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Coordinates;
+
+namespace Smart.ValueTypes.Benchmarks.Coordinates
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class LongitudeBenchmarks
+    {
+        [Benchmark]
+        public void Longitude_Constructor_Benchmark()
+        {
+            _ = new Longitude(0);
+        }
+
+        [Benchmark]
+        public void Longitude_From_Benchmark()
+        {
+            _ = Longitude.From(0);
+        }
+    }
+}
