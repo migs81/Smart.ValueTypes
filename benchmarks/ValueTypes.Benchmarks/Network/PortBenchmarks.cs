4bc5ae07ac451a091632fe9bdbe78f38bfe8cc86
migs81 <m.gschwandtner81@gmail.com>
2026-07-11 20:25:27 +0200

    perf: add benchmarks for Port value type

---
+++ benchmarks/ValueTypes.Benchmarks/Network/PortBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Network;
+
+namespace Smart.ValueTypes.Benchmarks.Network
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class PortBenchmarks
+    {
+        [Benchmark]
+        public void Port_Constructor_Benchmark()
+        {
+            _ = new Port(0);
+        }
+        
+        [Benchmark]
+        public void Port_From_Benchmark()
+        {
+            _ = Port.From(0);
+        }
+
+        [Benchmark]
+        public void Port_TryFrom_Benchmark()
+        {
+            _ = Port.TryFrom(0, out _);
+        }
+    }
+}
