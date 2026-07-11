dcde1c2e9fdf2ed394db396db9da53054989d19f
migs81 <m.gschwandtner81@gmail.com>
2026-07-11 16:42:50 +0200

    perf: add benchmark for Reaumur value type

---
+++ benchmarks/ValueTypes.Benchmarks/Temperatures/ReaumurBenchmarks.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Temperatures;
+
+namespace Smart.ValueTypes.Benchmarks.Temperatures
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class ReaumurBenchmarks
+    {
+        [Benchmark]
+        public void Reaumur_Constructor_Integer_Benchmark()
+        {
+            _ = new Reaumur(10d);
+        }
+    }
+}
