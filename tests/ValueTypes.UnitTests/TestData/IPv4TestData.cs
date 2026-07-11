130fe1a3aeacf3986184a17e7406de6bbdbcff93
migs81 <m.gschwandtner81@gmail.com>
2026-07-11 12:36:22 +0200

    test: update test data for IPs

--- tests/ValueTypes.UnitTests/TestData/IPv4TestData.cs
+++ tests/ValueTypes.UnitTests/TestData/IPv4TestData.cs
@@ -2,7 +2,7 @@ namespace Smart.ValueTypes.UnitTests.TestData;
 
 public static class IPv4TestData
 {
-    public const string TooShortValue = "255.255";
+    public const string TooShortValue = "1.1.1";
     public const string TooLongValue = "255.255.255.255.0";
     public const string StartsWithDotValue = ".127.0.0.1";
     public const string EndsWithDotValue = "127.0.0.1.";
