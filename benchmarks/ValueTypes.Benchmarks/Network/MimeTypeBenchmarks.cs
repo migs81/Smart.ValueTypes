87003856df8413fa90fb975683e42dc8f9170e14
migs81 <m.gschwandtner81@gmail.com>
2026-07-24 17:14:25 +0200

    perf: add benchmark for MimeType value type

---
+++ benchmarks/ValueTypes.Benchmarks/Network/MimeTypeBenchmarks.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Network;
+
+namespace Smart.ValueTypes.Benchmarks.Network
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    [SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+    public class MimeTypeBenchmarks
+    {
+        [Benchmark]
+        public void MimeType_Constructor_Benchmark()
+        {
+            _ = new MimeType("application/json");
+        }
+    }
+}
