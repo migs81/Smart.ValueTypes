7fdb69d21ca1e086f7b6b3ee46a88d9e0612f5bd
migs81 <m.gschwandtner81@gmail.com>
2026-07-24 19:43:00 +0200

    perf: add benchmark for NanoId value type

---
+++ benchmarks/ValueTypes.Benchmarks/IDs/NanoIdBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.ID;
+
+namespace Smart.ValueTypes.Benchmarks.IDs
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    [SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+    public class NanoIdBenchmarks
+    {
+        [Benchmark]
+        public void NanoId_Constructor_Benchmark()
+        {
+            _ = new NanoId("V1StGXR8_Z5jdHi6B-myT");
+        }
+
+        [Benchmark]
+        public void NanoId_From_Benchmark()
+        {
+            _ = NanoId.From("V1StGXR8_Z5jdHi6B-myT");
+        }
+        
+        [Benchmark]
+        public void NanoId_TryFrom_Benchmark()
+        {
+            _ = NanoId.TryFrom("V1StGXR8_Z5jdHi6B-myT", out _);
+        }
+    }
+}
