16308c15af391900025cfdc999dcc7d46ec3bbb1
migs81 <m.gschwandtner81@gmail.com>
2026-07-24 19:43:23 +0200

    test: add unit tests for NanoId value type

---
+++ tests/ValueTypes.UnitTests/ID/NanoIdTest.cs
@@ -0,0 +1,115 @@
+﻿using Smart.ValueTypes.Types.ID;
+using System;
+using Xunit;
+
+namespace Smart.ValueTypes.UnitTests.ID
+{
+    public class NanoIdTest
+    {
+        #region constructor
+
+        [Fact]
+        public void Constructor_NoInput_ShouldReturnDefaultObject()
+        {
+	        // act
+            var result = new NanoId();
+            
+            // assert
+            Assert.Equal(NanoId.Empty, result);
+        }
+
+        [Theory]
+        [InlineData("V1StGXR8_Z5jdHi6B-myT")]
+        [InlineData("x7Kp2mQ9fR4aL8NcD3wZb")]
+        [InlineData("nF5Qv8T2kL9Xc4HjR6mPa")]
+        public void Constructor_ValidInput_ShouldReturnObject(string input)
+        {
+            // act
+            var result = new NanoId(input);
+            
+            // assert
+            Assert.Equal(input, result);
+        }
+
+        [Theory]
+        [InlineData(null, typeof(ArgumentNullException))]
+        [InlineData("", typeof(ArgumentException))]
+        [InlineData("00000000000000000000", typeof(InvalidNanoIdException))]
+        [InlineData("0000000000000000000000", typeof(InvalidNanoIdException))]
+        [InlineData("+00000000000000000000", typeof(InvalidNanoIdException))]
+        public void Constructor_WrongInput_ShouldThrowException(string input, Type expectedException)
+        {
+	        // act
+            var result = Record.Exception(() => new NanoId(input));
+            
+            // assert
+            Assert.Equal(expectedException, result?.GetType());
+        }
+
+        #endregion
+        
+        #region From
+        
+        [Theory]
+        [InlineData("V1StGXR8_Z5jdHi6B-myT")]
+        [InlineData("x7Kp2mQ9fR4aL8NcD3wZb")]
+        [InlineData("nF5Qv8T2kL9Xc4HjR6mPa")]
+        public void From_ValidInput_ShouldReturnObject(string input)
+        {
+            // act
+            var result = NanoId.From(input);
+            
+            // assert
+            Assert.Equal(input, result);
+        }
+        
+        [Theory]
+        [InlineData(null, typeof(ArgumentNullException))]
+        [InlineData("", typeof(ArgumentException))]
+        [InlineData("00000000000000000000", typeof(InvalidNanoIdException))]
+        [InlineData("0000000000000000000000", typeof(InvalidNanoIdException))]
+        [InlineData("+00000000000000000000", typeof(InvalidNanoIdException))]
+        public void From_WrongInput_ShouldThrowException(string input, Type expectedException)
+        {
+	        // act
+            var result = Record.Exception(() => new NanoId(input));
+            
+            // assert
+            Assert.Equal(expectedException, result?.GetType());
+        }
+
+        #endregion
+        
+        #region From
+
+        [Theory]
+        [InlineData("V1StGXR8_Z5jdHi6B-myT")]
+        [InlineData("x7Kp2mQ9fR4aL8NcD3wZb")]
+        [InlineData("nF5Qv8T2kL9Xc4HjR6mPa")]
+        public void TryFrom_ValidInput_ShouldReturnOK(string input)
+        {
+            // act
+            var result = NanoId.TryFrom(input, out _);
+                
+            // assert
+            Assert.Equal(NanoId.Validation.Ok, result);
+        }
+        
+        [Theory]
+        [InlineData(null, NanoId.Validation.Null)]
+        [InlineData("", NanoId.Validation.Empty)]
+        [InlineData("00000000000000000000", NanoId.Validation.WrongLength)]
+        [InlineData("0000000000000000000000", NanoId.Validation.WrongLength)]
+        [InlineData("+00000000000000000000", NanoId.Validation.IllegalCharacter)]
+        public void TryFrom_WrongInput_ShouldReturnError(string input, NanoId.Validation expected)
+        {
+	        // act
+            var result = NanoId.TryFrom(input, out _);
+            
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        #endregion
+    }
+}
