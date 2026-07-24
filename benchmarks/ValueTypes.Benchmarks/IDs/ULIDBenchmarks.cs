8c6d9a96d9a7737fd696abc89e802b6e22edf9c1
migs81 <m.gschwandtner81@gmail.com>
2026-07-24 19:23:58 +0200

    perf: add benachmark for ULID value type

---
+++ benchmarks/ValueTypes.Benchmarks/IDs/ULIDBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.ID;
+
+namespace Smart.ValueTypes.Benchmarks.IDs
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    [SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+    public class ULIDBenchmarks
+    {
+        [Benchmark]
+        public void ULID_Constructor_Benchmark()
+        {
+            _ = new ULID("01J8R5T4W8M3F2X9QK7HCVN6PA");
+        }
+
+        [Benchmark]
+        public void ULID_From_Benchmark()
+        {
+            _ = ULID.From("01J8R5T4W8M3F2X9QK7HCVN6PA");
+        }
+        
+        [Benchmark]
+        public void ULID_TryFrom_Benchmark()
+        {
+            _ = ULID.TryFrom("01J8R5T4W8M3F2X9QK7HCVN6PA", out _);
+        }
+    }
+}
