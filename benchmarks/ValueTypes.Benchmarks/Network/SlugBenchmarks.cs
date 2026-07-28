446d60749c1799f116909966ff18514a05d4d7a1
migs81 <m.gschwandtner81@gmail.com>
2026-07-28 17:30:44 +0200

    perf: add benchmark for slug value type

---
+++ benchmarks/ValueTypes.Benchmarks/Network/SlugBenchmarks.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Network;
+
+namespace Smart.ValueTypes.Benchmarks.Network
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    [SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+    public class SlugBenchmarks
+    {
+        // [Benchmark]
+        // public void Slug_Constructor_Benchmark()
+        // {
+        //     _ = new Slug("test-slug");
+        // }
+        
+        [Benchmark]
+        public void Slug_Parse_Benchmark()
+        {
+            _ = Slug.Parse("Crème brûlée aus Österreich");
+        }
+    }
+}
