f33b50fb00ecaeb555f2f0befa8e38cb338a1b85
migs81 <m.gschwandtner81@gmail.com>
2026-07-28 22:26:04 +0200

    perf: add benchmarks for Opacity

---
+++ benchmarks/ValueTypes.Benchmarks/Graphics/OpacityBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Graphics;
+
+namespace Smart.ValueTypes.Benchmarks.Graphics
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class OpacityBenchmarks
+    {
+        [Benchmark]
+        public void Opacity_Constructor_Benchmark()
+        {
+            _ = new Opacity(0.75);
+        }
+
+        [Benchmark]
+        public void Opacity_From_Benchmark()
+        {
+            _ = Opacity.From(0.75);
+        }
+    }
+}
