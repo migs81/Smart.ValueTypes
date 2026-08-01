1591d564eb67803297684ee3ba52ac6c7d0823a1
migs81 <m.gschwandtner81@gmail.com>
2026-08-01 22:05:52 +0200

    perf: add benchmarks for WindowsFilePath

---
+++ benchmarks/ValueTypes.Benchmarks/IO/WindowsFilePathBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.IO;
+
+namespace Smart.ValueTypes.Benchmarks.IO
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class WindowsFilePathBenchmarks
+    {
+        [Benchmark]
+        public void WindowsFilePath_Constructor_Benchmark()
+        {
+            _ = new WindowsFilePath(@"C:\Temp\temp.log");
+        }
+        
+        [Benchmark]
+        public void WindowsFilePath_Parse_Benchmark()
+        {
+            _ = WindowsFilePath.Parse(@"C:\\Temp/temp.log");
+        }
+    }
+}
