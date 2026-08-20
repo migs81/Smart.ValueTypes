b2ade5c5cc8535aa1a8ede0be8a6a8881701f348
migs81 <m.gschwandtner81@gmail.com>
2026-08-20 18:55:09 +0200

    test: add unit test for new address value types

---
+++ tests/ValueTypes.UnitTests/Address/Austria/AustrianAddressCodeTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Smart.ValueTypes.Types.Address.Austria;
+using Xunit;
+
+namespace Smart.ValueTypes.UnitTests.Address.Austria
+{
+    public class AustrianAddressCodeTest
+    {
+        #region constructor
+
+        [Fact]
+        public void Constructor_NoInput_ShouldReturnDefaultObject()
+        {
+            // arrange
+            var expected = default(AustrianAddressCode);
+            
+            // act
+            var result = new AustrianAddressCode();
+            
+            // assert
+            Assert.Equal(expected, result);
+            Assert.True(result.IsDefault);
+        }
+
+        [Theory]
+        [InlineData("00000")]
+        [InlineData("99999")]
+        public void Constructor_ValidInput_ShouldReturnObject(string input)
+        {
+            // act
+            var result = new AustrianAddressCode(input);
+                
+            // assert
+            Assert.Equal(input, result);
+        }
+
+        [Theory]
+        [InlineData(null, typeof(ArgumentNullException))]
+        [InlineData("", typeof(ArgumentException))]
+        [InlineData("1", typeof(InvalidAustrianAddressCodeException))]
+        [InlineData("1234", typeof(InvalidAustrianAddressCodeException))]
+        [InlineData("123456", typeof(InvalidAustrianAddressCodeException))]
+        [InlineData("1234a", typeof(InvalidAustrianAddressCodeException))]
+        public void Constructor_WrongInput_ShouldThrowException(string input, Type expectedException)
+        {
+            // act
+            var result = Record.Exception(() => new AustrianAddressCode(input));
+            
+            // assert
+            Assert.Equal(expectedException, result?.GetType());
+        }
+
+        #endregion
+        
+        #region From
+        
+        [Theory]
+        [InlineData("00000")]
+        [InlineData("99999")]
+        public void From_ValidInput_ShouldReturnObject(string input)
+        {
+            // act
+            var result = AustrianAddressCode.From(input);
+                
+            // assert
+            Assert.Equal(input, result);
+        }
+        
+        [Theory]
+        [InlineData(null, typeof(ArgumentNullException))]
+        [InlineData("", typeof(ArgumentException))]
+        [InlineData("1", typeof(InvalidAustrianAddressCodeException))]
+        [InlineData("1234", typeof(InvalidAustrianAddressCodeException))]
+        [InlineData("123456", typeof(InvalidAustrianAddressCodeException))]
+        [InlineData("1234a", typeof(InvalidAustrianAddressCodeException))]
+        public void From_WrongInput_ShouldThrowException(string input, Type expectedException)
+        {
+            // act
+            var result = Record.Exception(() => AustrianAddressCode.From(input));
+            
+            // assert
+            Assert.Equal(expectedException, result?.GetType());
+        }
+        
+        #endregion
+        
+        #region TryFrom
+        
+        [Theory]
+        [InlineData("00000")]
+        [InlineData("99999")]
+        public void TryFrom_ValidInput_ShouldReturnOK(string input)
+        {
+            // act
+            var result = AustrianAddressCode.TryFrom(input, out _);
+                
+            // assert
+            Assert.Equal(AustrianAddressCode.Validation.Ok, result);
+        }
+        
+        [Theory]
+        [InlineData(null, AustrianAddressCode.Validation.Null)]
+        [InlineData("", AustrianAddressCode.Validation.Empty)]
+        [InlineData("1", AustrianAddressCode.Validation.InvalidLength)]
+        [InlineData("1234", AustrianAddressCode.Validation.InvalidLength)]
+        [InlineData("123456", AustrianAddressCode.Validation.InvalidLength)]
+        [InlineData("1234a", AustrianAddressCode.Validation.ContainsIllegalCharacter)]
+        public void TryFrom_WrongInput_ShouldReturnError(string input, AustrianAddressCode.Validation expected)
+        {
+            // act
+            var result = AustrianAddressCode.TryFrom(input, out _);
+            
+            // assert
+            Assert.Equal(expected, result);
+        }
+        
+        #endregion
+    }
+}
---
+++ tests/ValueTypes.UnitTests/Address/Austria/AustrianCadastralCommunityCodeTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Smart.ValueTypes.Types.Address.Austria;
+using Xunit;
+
+namespace Smart.ValueTypes.UnitTests.Address.Austria
+{
+    public class AustrianCadastralCommunityCodeTest
+    {
+        #region constructor
+
+        [Fact]
+        public void Constructor_NoInput_ShouldReturnDefaultObject()
+        {
+            // arrange
+            var expected = default(AustrianCadastralCommunityCode);
+            
+            // act
+            var result = new AustrianCadastralCommunityCode();
+            
+            // assert
+            Assert.Equal(expected, result);
+            Assert.True(result.IsDefault);
+        }
+
+        [Theory]
+        [InlineData("00000")]
+        [InlineData("99999")]
+        public void Constructor_ValidInput_ShouldReturnObject(string input)
+        {
+            // act
+            var result = new AustrianCadastralCommunityCode(input);
+                
+            // assert
+            Assert.Equal(input, result);
+        }
+
+        [Theory]
+        [InlineData(null, typeof(ArgumentNullException))]
+        [InlineData("", typeof(ArgumentException))]
+        [InlineData("1", typeof(InvalidAustrianCadastralCommunityCodeException))]
+        [InlineData("1234", typeof(InvalidAustrianCadastralCommunityCodeException))]
+        [InlineData("123456", typeof(InvalidAustrianCadastralCommunityCodeException))]
+        [InlineData("1234a", typeof(InvalidAustrianCadastralCommunityCodeException))]
+        public void Constructor_WrongInput_ShouldThrowException(string input, Type expectedException)
+        {
+            // act
+            var result = Record.Exception(() => new AustrianCadastralCommunityCode(input));
+            
+            // assert
+            Assert.Equal(expectedException, result?.GetType());
+        }
+
+        #endregion
+        
+        #region From
+        
+        [Theory]
+        [InlineData("00000")]
+        [InlineData("99999")]
+        public void From_ValidInput_ShouldReturnObject(string input)
+        {
+            // act
+            var result = AustrianCadastralCommunityCode.From(input);
+                
+            // assert
+            Assert.Equal(input, result);
+        }
+        
+        [Theory]
+        [InlineData(null, typeof(ArgumentNullException))]
+        [InlineData("", typeof(ArgumentException))]
+        [InlineData("1", typeof(InvalidAustrianCadastralCommunityCodeException))]
+        [InlineData("1234", typeof(InvalidAustrianCadastralCommunityCodeException))]
+        [InlineData("123456", typeof(InvalidAustrianCadastralCommunityCodeException))]
+        [InlineData("1234a", typeof(InvalidAustrianCadastralCommunityCodeException))]
+        public void From_WrongInput_ShouldThrowException(string input, Type expectedException)
+        {
+            // act
+            var result = Record.Exception(() => AustrianCadastralCommunityCode.From(input));
+            
+            // assert
+            Assert.Equal(expectedException, result?.GetType());
+        }
+        
+        #endregion
+        
+        #region TryFrom
+        
+        [Theory]
+        [InlineData("00000")]
+        [InlineData("99999")]
+        public void TryFrom_ValidInput_ShouldReturnOK(string input)
+        {
+            // act
+            var result = AustrianCadastralCommunityCode.TryFrom(input, out _);
+                
+            // assert
+            Assert.Equal(AustrianCadastralCommunityCode.Validation.Ok, result);
+        }
+        
+        [Theory]
+        [InlineData(null, AustrianCadastralCommunityCode.Validation.Null)]
+        [InlineData("", AustrianCadastralCommunityCode.Validation.Empty)]
+        [InlineData("1", AustrianCadastralCommunityCode.Validation.InvalidLength)]
+        [InlineData("1234", AustrianCadastralCommunityCode.Validation.InvalidLength)]
+        [InlineData("123456", AustrianCadastralCommunityCode.Validation.InvalidLength)]
+        [InlineData("1234a", AustrianCadastralCommunityCode.Validation.ContainsIllegalCharacter)]
+        public void TryFrom_WrongInput_ShouldReturnError(string input, AustrianCadastralCommunityCode.Validation expected)
+        {
+            // act
+            var result = AustrianCadastralCommunityCode.TryFrom(input, out _);
+            
+            // assert
+            Assert.Equal(expected, result);
+        }
+        
+        #endregion
+    }
+}
