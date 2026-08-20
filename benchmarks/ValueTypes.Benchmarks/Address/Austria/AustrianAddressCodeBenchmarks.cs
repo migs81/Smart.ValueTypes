2c14af70a66a8ed67c377d31e7ef9ab5c8a81143
migs81 <m.gschwandtner81@gmail.com>
2026-08-20 18:55:26 +0200

    perf: add benchmarks for new address value types

---
+++ benchmarks/ValueTypes.Benchmarks/Address/Austria/AustrianAddressCodeBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Address.Austria;
+
+namespace Smart.ValueTypes.Benchmarks.Address.Austria
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class AustrianAddressCodeBenchmarks
+    {
+        [Benchmark]
+        public void AustrianAddressCode_Constructor_Benchmark()
+        {
+            _ = new AustrianAddressCode("12345");
+        }
+
+        [Benchmark]
+        public void AustrianAddressCode_From_Benchmark()
+        {
+            _ = AustrianAddressCode.From("12345");
+        }
+
+        [Benchmark]
+        public void AustrianAddressCode_TryFrom_Benchmark()
+        {
+            _ = AustrianAddressCode.TryFrom("12345", out _);
+        }
+    }
+}
---
+++ benchmarks/ValueTypes.Benchmarks/Address/Austria/AustrianCadastralCommunityCodeBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Address.Austria;
+
+namespace Smart.ValueTypes.Benchmarks.Address.Austria
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class AustrianCadastralCommunityCodeBenchmarks
+    {
+        [Benchmark]
+        public void AustrianCadastralCommunityCode_Constructor_Benchmark()
+        {
+            _ = new AustrianCadastralCommunityCode("12345");
+        }
+
+        [Benchmark]
+        public void AustrianCadastralCommunityCode_From_Benchmark()
+        {
+            _ = AustrianCadastralCommunityCode.From("12345");
+        }
+
+        [Benchmark]
+        public void AustrianCadastralCommunityCode_TryFrom_Benchmark()
+        {
+            _ = AustrianCadastralCommunityCode.TryFrom("12345", out _);
+        }
+    }
+}
