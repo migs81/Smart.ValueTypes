730ff91e78f5fc0d1c52270b9a357efadf65aaef
migs81 <m.gschwandtner81@gmail.com>
2026-08-20 17:29:39 +0200

    perf: add benchmarks for new value type AustrianPostalCode

---
+++ benchmarks/ValueTypes.Benchmarks/Address/Austria/AustrianPostalCodeBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Address.Austria;
+
+namespace Smart.ValueTypes.Benchmarks.Address.Austria
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class AustrianPostalCodeBenchmarks
+    {
+        [Benchmark]
+        public void AustrianPostalCode_Constructor_Benchmark()
+        {
+            _ = new AustrianPostalCode("1120");
+        }
+
+        [Benchmark]
+        public void AustrianPostalCode_From_Benchmark()
+        {
+            _ = AustrianPostalCode.From("1120");
+        }
+
+        [Benchmark]
+        public void AustrianPostalCode_TryFrom_Benchmark()
+        {
+            _ = AustrianPostalCode.TryFrom("1120", out _);
+        }
+    }
+}
