04329d4fb2cd082eeec6364eeab17bceb0d7d3fb
migs81 <m.gschwandtner81@gmail.com>
2026-08-22 23:24:16 +0200

    perf: add benchmarks for value type NonEmptyString

---
+++ benchmarks/ValueTypes.Benchmarks/Text/NonEmptyStringCodeBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Text;
+
+namespace Smart.ValueTypes.Benchmarks.Text
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class NonEmptyStringBenchmarks
+    {
+        [Benchmark]
+        public void NonEmptyString_Constructor_Benchmark()
+        {
+            _ = new NonEmptyString("12345");
+        }
+
+        [Benchmark]
+        public void NonEmptyString_From_Benchmark()
+        {
+            _ = NonEmptyString.From("12345");
+        }
+
+        [Benchmark]
+        public void NonEmptyString_TryFrom_Benchmark()
+        {
+            _ = NonEmptyString.TryFrom("12345", out _);
+        }
+    }
+}
