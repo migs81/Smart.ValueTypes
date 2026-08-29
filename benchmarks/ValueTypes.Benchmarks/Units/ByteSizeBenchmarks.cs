52027b955190172be133764b36682f31ef98d336
migs81 <m.gschwandtner81@gmail.com>
2026-08-29 18:51:41 +0200

    perf: add benchmarks for the new value type ByteSize

---
+++ benchmarks/ValueTypes.Benchmarks/Units/ByteSizeBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Units;
+
+namespace Smart.ValueTypes.Benchmarks.Units
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    public class ByteSizeBenchmarks
+    {
+        [Benchmark]
+        public void ByteSize_Constructor_Benchmark()
+        {
+            _ = new ByteSize(1_234_567_890);
+        }
+        
+        [Benchmark]
+        public void ByteSize_FromTebibytes_Benchmark()
+        {
+            _ = ByteSize.FromTebibytes(10);
+        }
+    }
+}
