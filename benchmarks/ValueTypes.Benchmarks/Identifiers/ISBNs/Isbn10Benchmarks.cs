44ecc9da434e3c702f67bb62717d0c721a10f646
migs81 <m.gschwandtner81@gmail.com>
2026-09-11 23:33:30 +0200

    perf: add benchmarks for ISBN10 and ISBN13

---
+++ benchmarks/ValueTypes.Benchmarks/Identifiers/ISBNs/Isbn10Benchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Identifiers.ISBNs;
+
+namespace Smart.ValueTypes.Benchmarks.Identifiers.ISBNs
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    [SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+    public class Isbn10Benchmarks
+    {
+        [Benchmark]
+        public void ISBN10_Constructor_Benchmark()
+        {
+            _ = new ISBN10("0-131-10362-8");
+        }
+
+        [Benchmark]
+        public void ISBN10_From_Benchmark()
+        {
+            _ = ISBN10.From("0-131-10362-8");
+        }
+        
+        [Benchmark]
+        public void ISBN10_TryFrom_Benchmark()
+        {
+            _ = ISBN10.TryFrom("0-131-10362-8", out _);
+        }
+    }
+}
---
+++ benchmarks/ValueTypes.Benchmarks/Identifiers/ISBNs/Isbn13Benchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Identifiers.ISBNs;
+
+namespace Smart.ValueTypes.Benchmarks.Identifiers.ISBNs
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    [SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+    public class Isbn13Benchmarks
+    {
+        [Benchmark]
+        public void ISBN13_Constructor_Benchmark()
+        {
+            _ = new ISBN13("978-0-201-61622-4");
+        }
+
+        [Benchmark]
+        public void ISBN13_From_Benchmark()
+        {
+            _ = ISBN13.From("978-0-201-61622-4");
+        }
+        
+        [Benchmark]
+        public void ISBN13_TryFrom_Benchmark()
+        {
+            _ = ISBN13.TryFrom("978-0-201-61622-4", out _);
+        }
+    }
+}
