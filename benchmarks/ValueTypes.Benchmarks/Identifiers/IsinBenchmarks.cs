7ed0a8158d949936c5605b5b2a7a612a1b4b59df
migs81 <m.gschwandtner81@gmail.com>
2026-09-12 16:16:00 +0200

    perf: add benchmarks for value type ISIN

---
+++ benchmarks/ValueTypes.Benchmarks/Identifiers/IsinBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Identifiers;
+
+namespace Smart.ValueTypes.Benchmarks.Identifiers
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    [SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+    public class IsinBenchmarks
+    {
+        [Benchmark]
+        public void ISIN_Constructor_Benchmark()
+        {
+            _ = new ISIN("AT0000APOST4");
+        }
+
+        [Benchmark]
+        public void ISIN_From_Benchmark()
+        {
+            _ = ISIN.From("AT0000APOST4");
+        }
+        
+        [Benchmark]
+        public void ISIN_TryFrom_Benchmark()
+        {
+            _ = ISIN.TryFrom("AT0000APOST4", out _);
+        }
+    }
+}
