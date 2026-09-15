32f7c3960f8a2f2ef99c32db30e35e63af436b7a
migs81 <m.gschwandtner81@gmail.com>
2026-09-15 18:09:15 +0200

    perf: add benchmarks for value type WKN

---
+++ benchmarks/ValueTypes.Benchmarks/Identifiers/WknBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using Smart.ValueTypes.Types.Identifiers;
+
+namespace Smart.ValueTypes.Benchmarks.Identifiers
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical)]
+    [SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+    public class WknBenchmarks
+    {
+        [Benchmark]
+        public void WKN_Constructor_Benchmark()
+        {
+            _ = new WKN("581005");
+        }
+
+        [Benchmark]
+        public void WKN_From_Benchmark()
+        {
+            _ = WKN.From("581005");
+        }
+        
+        [Benchmark]
+        public void WKN_TryFrom_Benchmark()
+        {
+            _ = WKN.TryFrom("581005", out _);
+        }
+    }
+}
